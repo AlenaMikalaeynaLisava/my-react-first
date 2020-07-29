4d5f1166334f2e2d44644bd917238dc5378aee9b
AlenaMikalaeynaLisava <63248586+AlenaMikalaeynaLisava@users.noreply.github.com>
2020-07-30 00:24:42 +0300

    FirstTask

---
+++ src/ClickerCounter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Site({ number }) {
+  if (number%2==0){
+    return <div className="red"> Counter {number}</div>;
+  } else return <div className="aqua"> Counter {number}</div>;
+}
